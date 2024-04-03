d77c654dc13bc33f73d5903b11ed1ed2ce6e4efe
hericlibong <hericlibong@gmail.com>
2024-04-03 11:53:50 +0200

    modified player model

--- ChessTournamentAPP/models/player.py
+++ ChessTournamentAPP/models/player.py
@@ -5,6 +5,7 @@ et un identifiant national d'échecs unique.
 Ces informations seront stockées dans des fichiers JSON.
 
 """
+import re
 
 
 class Player:
@@ -14,7 +15,11 @@ class Player:
         self.name = name
         self.firstname = firstname
         self.birthdate = birthdate
+        # Validation de l'unique_id
+        if not re.match(r'^[A-Z]{2}\d{5}$', unique_id):
+            raise ValueError("L'unique_id doit suivre le format : deux lettres suivies de cinq chiffres.")
         self.unique_id = unique_id
+        
 
 
         

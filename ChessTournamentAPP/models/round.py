068a6a6abf069c22b3752f168ecdde705c84ac2b
hericlibong <hericlibong@gmail.com>
2024-04-03 11:54:44 +0200

    modified round model

--- ChessTournamentAPP/models/round.py
+++ ChessTournamentAPP/models/round.py
@@ -6,8 +6,9 @@ from match import Match
 class Round:
     """Représentation d'un tour dans un tournoi d'échecs."""
     def __init__(self, name: str, start_time: datetime = None, end_time: datetime = None):
+        """Initialise  le nom, la date de début et la date de fin d'un tour dans un tournoi """
         self.name = name
-        self.start_time = start_time if start_time else datetime.now()
+        self.start_time = start_time if start_time else datetime.now() # Un tour commence immédiatement par défaut si aucune heure de début n'est spécifiée
         self.end_time = end_time
         self.matches = []  # type: List[Match]
 

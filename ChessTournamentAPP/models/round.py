f9bab11df795e0da9df593dcd2e6cd29675894fd
hericlibong <hericlibong@gmail.com>
2024-04-15 02:05:08 +0200

    set model attribut is.complete

--- ChessTournamentAPP/models/round.py
+++ ChessTournamentAPP/models/round.py
@@ -1,7 +1,9 @@
+#models/round.py
 
 from datetime import datetime
-from typing import List
+from typing import List, Tuple
 from .match import Match
+from .player import Player
 
 class Round:
     """Représentation d'un tour dans un tournoi d'échecs."""
@@ -13,17 +15,30 @@ class Round:
         self.matches = []  # type: List[Match]
         self.is_complete = is_complete  # Attribut ajouté en guise de test
 
-    def add_match(self, match):
-        """Ajoute un match à la liste des matchs du tour."""
+
+
+    def add_match(self, player1:Player, player2:Player, results: Tuple[float, float] = (0, 0)):
+        """Ajoute un match au round avec les joueurs et les résultats spécifiés"""
+        match = Match(players=(player1, player2), results=results)
         if not self.is_complete:
             self.matches.append(match)
+            print(f"Le match entre {player1.firstname} {player1.name} and {player2.firstname} {player2.name} a été ajouté au round '{self.name}")
         else:
-            print("Impossible d'ajouter un match à un tour complet")
+            print("Cannot add match to a completed round")
+
+    
+
+    # def add_match(self, match):
+    #     """Ajoute un match à la liste des matchs du tour."""
+    #     if not self.is_complete:
+    #         self.matches.append(match)
+    #     else:
+    #         print("Impossible d'ajouter un match à un tour complet")
 
 
-    # Ajouté en guise de test
-    def complete_round(self):
-        self.is_complete = True
-        self.end_time = datetime.now()
+    # # Ajouté en guise de test
+    # def complete_round(self):
+    #     self.is_complete = True
+    #     self.end_time = datetime.now()
 
     

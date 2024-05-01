2c7fae53e31fafb44586bb5499b648a43d180883
hericlibong <hericlibong@gmail.com>
2024-05-02 00:59:37 +0200

    added last table

--- ChessTournamentAPP/controllers/round_controller.py
+++ ChessTournamentAPP/controllers/round_controller.py
@@ -1,33 +1,5 @@
-# from controllers.base_controller import BaseController
-# from views.menu_view import MenuView
-# from views.round_views import RoundView
 
-
-# class RoundController(BaseController):
-
-
-    # def manage_rounds(self, tournament):
-    #         while True:
-    #             choice = MenuView.display_round_menu()
-    #             if choice == '1':
-    #                 round_name = RoundView.create_round_info()
-    #                 tournament.add_round(round_name)
-    #             elif choice == '2':
-    #                 round_index = RoundView.select_round_to_start(tournament)
-    #                 tournament.start_round(round_index)
-    #             elif choice == '3':
-    #                 round_index = RoundView.select_round_to_end(tournament)
-    #                 if round_index is not None:
-    #                     for match in tournament.rounds[round_index].matches:
-    #                         print(match.display_match())
-    #                         match_results = RoundView.get_match_results()
-    #                         match.set_results(match_results)
-    #                     tournament.rounds[round_index].end_round()
-    #             elif choice == '4':
-    #                 break
-
-
-    # controllers/round_controller.py
+# controllers/round_controller.py
 
 from controllers.base_controller import BaseController
 from views.menu_view import MenuView
